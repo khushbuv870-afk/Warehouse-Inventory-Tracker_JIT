bcc353ce03f8539c85bb2b43ee280a73b3af13ed
khushbu vishwakarma <khushbuv870>
2025-10-18 10:02:32 +0530

    khushbu

---
+++ Product.java
@@ -0,0 +1,33 @@
+public class Product {
+    private String productId;
+    private String name;
+    private int quantity;
+    private int reorderThreshold;
+
+    public Product(String productId, String name, int quantity, int reorderThreshold) {
+        this.productId = productId;
+        this.name = name;
+        this.quantity = quantity;
+        this.reorderThreshold = reorderThreshold;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getReorderThreshold() {
+        return reorderThreshold;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
